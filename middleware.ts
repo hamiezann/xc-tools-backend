149b04a64a02d2e5ba61ae518c4c81cf13581633
hamiezann <136826504+hamiezann@users.noreply.github.com>
2025-09-29 02:17:19 +0800

    add url in middleware

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,8 @@ export function middleware(req: NextRequest) {
       status: 200,
       headers: {
         "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Origin": "http://localhost:5173", // your frontend
+        // "Access-Control-Allow-Origin": "http://localhost:5173", // your frontend
+        "Access-Control-Allow-Origin": "https://xc-tools.vercel.app", // your frontend
         "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT,OPTIONS",
         "Access-Control-Allow-Headers":
           "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
@@ -20,7 +21,8 @@ export function middleware(req: NextRequest) {
   // Add CORS headers to all other API responses
   const res = NextResponse.next();
   res.headers.set("Access-Control-Allow-Credentials", "true");
-  res.headers.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  // res.headers.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.headers.set("Access-Control-Allow-Origin", "https://xc-tools.vercel.app");
   res.headers.set(
     "Access-Control-Allow-Methods",
     "GET,DELETE,PATCH,POST,PUT,OPTIONS"
