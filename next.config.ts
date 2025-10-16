92d51402487ea82ff71c54160ae888566f315a6c
hamiezann <136826504+hamiezann@users.noreply.github.com>
2025-10-16 11:25:00 +0800

    Comment out headers function in next.config.ts
    Comment out the async headers function in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            // value: "http://localhost:5173",
-            value: "https://xc-tools.onrender.com",
-          }, // Vite default port
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           // value: "http://localhost:5173",
+  //           value: "https://xc-tools.onrender.com",
+  //         }, // Vite default port
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET,DELETE,PATCH,POST,PUT",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
